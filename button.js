a0b033dec040cab93a4cb51bf275557d79b4918f
hudaMjM <168384919+hudaMjM@users.noreply.github.com>
2024-04-29 08:40:11 +0300

    Add files via upload

---
+++ button.js
@@ -0,0 +1,149 @@
+let count=0;
+document.getElementById("increaseButton").onclick=function(){
+count+=1;
+document.getElementById("countlable").innerHTML=count;
+}
+document.getElementById("resetButton").onclick=function(){
+  count=0;  
+  document.getElementById("countlable").innerHTML=count;
+}
+
+//--------
+let count1=0;
+document.getElementById("increaseButton1").onclick=function(){
+count1+=1;
+document.getElementById("countlable1").innerHTML=count1;
+}
+document.getElementById("resetButton1").onclick=function(){
+  count1=0;  
+  document.getElementById("countlable1").innerHTML=count1;}
+//--------
+let count2=0;
+document.getElementById("increaseButton2").onclick=function(){
+count2+=1;
+document.getElementById("countlable2").innerHTML=count2;
+}
+document.getElementById("resetButton2").onclick=function(){
+  count2=0;  
+  document.getElementById("countlable2").innerHTML=count2;
+}
+//--------
+let count3=0;
+document.getElementById("increaseButton3").onclick=function(){
+count3+=1;
+document.getElementById("countlable3").innerHTML=count3;
+}
+document.getElementById("resetButton3").onclick=function(){
+  count3=0;  
+  document.getElementById("countlable3").innerHTML=count3;
+}
+//--------
+let count4=0;
+document.getElementById("increaseButton4").onclick=function(){
+count4+=1;
+document.getElementById("countlable4").innerHTML=count4;
+}
+document.getElementById("resetButton4").onclick=function(){
+  count4=0;  
+  document.getElementById("countlable4").innerHTML=count4;
+}
+//--------
+let count5=0;
+document.getElementById("increaseButton5").onclick=function(){
+count5+=1;
+document.getElementById("countlable5").innerHTML=count5;
+}
+document.getElementById("resetButton5").onclick=function(){
+  count5=0;  
+  document.getElementById("countlable5").innerHTML=count5;
+}
+//--------
+let count6=0;
+document.getElementById("increaseButton6").onclick=function(){
+count6+=1;
+document.getElementById("countlable6").innerHTML=count6;
+}
+document.getElementById("resetButton6").onclick=function(){
+  count6=0;  
+  document.getElementById("countlable6").innerHTML=count6;
+}
+//--------
+let count7=0;
+document.getElementById("increaseButton7").onclick=function(){
+count7+=1;
+document.getElementById("countlable7").innerHTML=count7;
+}
+document.getElementById("resetButton7").onclick=function(){
+  count7=0;  
+  document.getElementById("countlable7").innerHTML=count7;
+}
+//--------
+let count8=0;
+document.getElementById("increaseButton8").onclick=function(){
+count8+=1;
+document.getElementById("countlable8").innerHTML=count8;
+}
+document.getElementById("resetButton8").onclick=function(){
+  count8=0;  
+  document.getElementById("countlable8").innerHTML=count8;
+}
+//--------
+let count9=0;
+document.getElementById("increaseButton9").onclick=function(){
+count9+=1;
+document.getElementById("countlable9").innerHTML=count9;
+}
+document.getElementById("resetButton9").onclick=function(){
+  count9=0;  
+  document.getElementById("countlable9").innerHTML=count9;
+}
+//--------
+let count10=0;
+document.getElementById("increaseButton10").onclick=function(){
+count10+=1;
+document.getElementById("countlable10").innerHTML=count10;
+}
+document.getElementById("resetButton10").onclick=function(){
+  count10=0;  
+  document.getElementById("countlable10").innerHTML=count10;
+}
+//--------
+let count11=0;
+document.getElementById("increaseButton11").onclick=function(){
+count11+=1;
+document.getElementById("countlable11").innerHTML=count11;
+}
+document.getElementById("resetButton11").onclick=function(){
+  count11=0;  
+  document.getElementById("countlable11").innerHTML=count11;
+}
+//--------
+let count12=0;
+document.getElementById("increaseButton12").onclick=function(){
+count12+=1;
+document.getElementById("countlable12").innerHTML=count12;
+}
+document.getElementById("resetButton12").onclick=function(){
+  count12=0;  
+  document.getElementById("countlable12").innerHTML=count12;
+}
+//--------
+let count13=0;
+document.getElementById("increaseButton13").onclick=function(){
+count13+=1;
+document.getElementById("countlable13").innerHTML=count13;
+}
+document.getElementById("resetButton13").onclick=function(){
+  count13=0;  
+  document.getElementById("countlable13").innerHTML=count13;
+}
+//--------
+let count14=0;
+document.getElementById("increaseButton14").onclick=function(){
+count14+=1;
+document.getElementById("countlable14").innerHTML=count14;
+}
+document.getElementById("resetButton14").onclick=function(){
+  count14=0;  
+  document.getElementById("countlable14").innerHTML=count14;
+}
